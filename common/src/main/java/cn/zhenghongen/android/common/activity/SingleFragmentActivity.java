a6c23aad3f080a117f8be24e535faf9182471fa7
陌羽蕲moyuqi <zhenghongen@gmail.com>
2015-12-22 13:39:56 +0800

    layout 和 fragment id 改为可配置

--- common/src/main/java/cn/zhenghongen/android/common/activity/SingleFragmentActivity.java
+++ common/src/main/java/cn/zhenghongen/android/common/activity/SingleFragmentActivity.java
@@ -7,18 +7,35 @@ import android.os.Bundle;
 import cn.zhenghongen.android.common.R;
 
 public abstract class SingleFragmentActivity extends BaseActivity {
+    /**
+     * @return
+     */
     protected abstract Fragment createFragment();
+
+    /**
+     * @return
+     */
+    protected int getLayoutResId() {
+        return R.layout.activity_single_fragment;
+    }
+
+    /**
+     * @return
+     */
+    protected int getFragmentId() {
+        return R.id.fragmentContainer;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        Fragment fragment = fm.findFragmentById(getFragmentId());
         if (fragment == null) {
             fragment = createFragment();
-            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+            fm.beginTransaction().add(getFragmentId(), fragment).commit();
         }
     }
 }
