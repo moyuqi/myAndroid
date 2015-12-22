1578245f9f577cdda837c1989e23c384b75e76e2
陌羽蕲moyuqi <zhenghongen@gmail.com>
2015-12-22 13:37:14 +0800

    1.封装一个Logger类

---
+++ common/src/main/java/cn/zhenghongen/android/common/util/Logger.java
@@ -0,0 +1,63 @@
+package cn.zhenghongen.android.common.util;
+
+import android.util.Log;
+
+import cn.zhenghongen.android.common.BuildConfig;
+
+
+/**
+ * Created by ZhengHongEn on 2015/12/22.
+ */
+public final class Logger {
+    // TODO: 2015/12/22 在项目中使用Logger的时候，根据项目来判断DEBUG？ 
+    private static final boolean DEBUG = BuildConfig.DEBUG;
+
+    public static void v(String tag, String msg) {
+        Log.v(tag, msg);
+    }
+
+    public static void v(String tag, String msg, Throwable tr) {
+        Log.v(tag, msg, tr);
+    }
+
+    public static void d(String tag, String msg) {
+        if (DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg, Throwable tr) {
+        if (DEBUG) {
+            Log.d(tag, msg, tr);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        Log.i(tag, msg);
+    }
+
+    public static void i(String tag, String msg, Throwable tr) {
+        Log.i(tag, msg, tr);
+    }
+
+    public static void w(String tag, String msg) {
+        Log.w(tag, msg);
+    }
+
+    public static void w(String tag, String msg, Throwable tr) {
+        Log.w(tag, msg, tr);
+    }
+
+    public static void w(String tag, Throwable tr) {
+        Log.w(tag, tr);
+    }
+
+    public static void e(String tag, String msg) {
+        Log.e(tag, msg);
+    }
+
+    public static void e(String tag, String msg, Throwable tr) {
+        Log.e(tag, msg, tr);
+    }
+
+}
