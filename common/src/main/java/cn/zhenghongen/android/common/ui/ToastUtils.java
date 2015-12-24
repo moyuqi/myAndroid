c1a557db64ef9cc7312026c12fa2ccc06f0e84d3
陌羽蕲moyuqi <zhenghongen@gmail.com>
2015-12-24 10:11:30 +0800

    1.调整参数名

--- common/src/main/java/cn/zhenghongen/android/common/ui/ToastUtils.java
+++ common/src/main/java/cn/zhenghongen/android/common/ui/ToastUtils.java
@@ -3,34 +3,32 @@ package cn.zhenghongen.android.common.ui;
 import android.content.Context;
 import android.widget.Toast;
 
-import cn.zhenghongen.android.common.Application;
-
 
 /**
  * Created by ZhengHongEn on 2015/11/2.
  */
 public class ToastUtils {
-    private static Context mcontext;
+    private static Context mContext;
 
     public static void init(Context context) {
-        mcontext = context;
+        mContext = context;
     }
 
     public static void shortToast(String text) {
-        Toast.makeText(mcontext, text, Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
     }
 
 
     public static void shortToast(int resid) {
-        Toast.makeText(mcontext, resid, Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, resid, Toast.LENGTH_SHORT).show();
     }
 
 
     public static void longToast(String text) {
-        Toast.makeText(mcontext, text, Toast.LENGTH_LONG).show();
+        Toast.makeText(mContext, text, Toast.LENGTH_LONG).show();
     }
 
     public static void longToast(int resid) {
-        Toast.makeText(mcontext, resid, Toast.LENGTH_LONG).show();
+        Toast.makeText(mContext, resid, Toast.LENGTH_LONG).show();
     }
 }
